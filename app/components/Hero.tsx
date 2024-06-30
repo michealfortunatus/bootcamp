5b629f24a9548bf62790d9c48dfd388a509ac298
Your Name <you@example.com>
2024-06-30 13:17:36 +0100

    Update Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
 import BackgroundImageLarge from '../assets/background3.png';
-import BackgroundImageSmall from '../assets/mobile-bg.png'; 
+import BackgroundImageSmall from '../assets/mobile-bg.png';
 
 const Hero: React.FC = () => {
   return (
-    <div className="relative text-white py-20 px-4 text-center md:text-left cursor-pointer">
+    <div className="relative text-white py-20 px-4 text-center md:text-left cursor-pointer min-h-screen">
       <div
         className="absolute inset-0 bg-cover bg-center hidden md:block"
-        style={{ backgroundImage: `url(${BackgroundImageLarge.src})` }}
+        style={{ backgroundImage: `url(${BackgroundImageLarge.src})`, backgroundSize: 'cover' }}
       ></div>
       <div
         className="absolute inset-0 bg-cover bg-center block md:hidden"
-        style={{ backgroundImage: `url(${BackgroundImageSmall.src})`, backgroundSize: 'contain' }}
+        style={{ backgroundImage: `url(${BackgroundImageSmall.src})`, backgroundSize: 'cover' }}
       ></div>
       <div className="container mx-auto flex flex-col md:flex-row items-center relative z-10">
         <div className="flex-1 mt-10 md:mt-0">
--- app/components/Subscribe.tsx
+++ app/components/Subscribe.tsx
@@ -13,7 +13,7 @@ const Subscribe: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center bg-white p-10 shadow-md rounded-lg">
-      <h2 className=" mb-2 w-full text-center text-black font-montserrat text-xl sm:text-2xl md:text-3xl font-bold leading-normal">Email capture form for updates and promotions</h2>
+      <h2 className="mb-2 w-full text-center text-black font-montserrat text-xl sm:text-2xl md:text-3xl font-bold leading-normal">Email capture form for updates and promotions</h2>
       <p className="text-black font-montserrat text-lg font-normal leading-normal text-opacity-75 text-center mb-6">
         Subscribe to our newsletters and stay up to date on features and releases.
       </p>
@@ -22,18 +22,18 @@ const Subscribe: React.FC = () => {
           {successMessage}
         </div>
       )}
-      <form className="flex w-full  rounded-full border-2 border-black border-opacity-75" onSubmit={handleSubmit}>
+      <form className="flex w-full rounded-full border-2 border-black border-opacity-75" onSubmit={handleSubmit}>
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter your work email"
-          className="flex-grow p-3 rounded-full"
+          className="flex-grow p-3 rounded-l-full"
           required
         />
         <button
           type="submit"
-          className="p-3  bg-[#031428] text-white border-2 border-black rounded-full "
+          className="p-3 bg-[#031428] text-white border-2 border-black rounded-r-full"
         >
           Subscribe
         </button>
