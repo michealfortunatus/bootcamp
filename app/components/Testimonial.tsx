22c26911d2b972ce50e99b9d534b79dda950d385
Your Name <you@example.com>
2024-06-30 12:07:12 +0100

    Update Testimonials

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -9,7 +9,7 @@ const Testimonial: React.FC = () => {
         <h2 className="text-sm text-[#8D8D8D] font-montserrat text-lg font-medium leading-relaxed mb-2 w-[185px]">Testimonials</h2>
         <h3 className="text-3xl font-bold mb-4 text-black font-montserrat ">See what others are saying about us</h3>
         <p className="mb-6 w-[601px] text-black font-montserrat text-lg font-normal leading-loose">
-          "Explore the course catalog and syllables to identify courses that align with personal learning objectives. Take advantage of interactive tutorials we offer to reinforce your understanding and apply concepts in real world scenarios."
+          &quot Explore the course catalog and syllables to identify courses that align with personal learning objectives. Take advantage of interactive tutorials we offer to reinforce your understanding and apply concepts in real world scenarios.&quot
         </p>
         <p className="font-bold w-[203px] text-black font-montserrat text-xl  leading-normal">Faaruq Azeez</p>
         <p className="mb-6 w-[273px] text-black font-montserrat text-xl font-normal leading-normal">
